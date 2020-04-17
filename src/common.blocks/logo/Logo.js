d9dca723dcc4c4bc559c19a51926ab2a09c79ded
shootingersNew <lebedevseergey@gmail.com>
2020-04-17 16:14:18 +0300

    fixed import

--- src/common.blocks/logo/Logo.js
+++ src/common.blocks/logo/Logo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "react-router-dom/modules/Link";
+import {Link} from "react-router-dom";
 import logo from './Logo.svg'
 
 export default function Logo() {
