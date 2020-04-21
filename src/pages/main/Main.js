088f39de661949512ada36a4ef3891d8bf76f69b
shootingersNew <lebedevseergey@gmail.com>
2020-04-21 13:22:46 +0300

    add jsx of header and slick to main page

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,6 +1,10 @@
 import React from "react";
+import Header from "../../common.blocks/header/Header";
+import Slides from "../../common.blocks/slider/Slider";
 import './../../common.blocks/page/page.css'
-import './../../common.blocks/fonts/__proximaNovaRegular/fonts__proximaNovaRegular.css'
+import font from './../../common.blocks/fonts/__proximaNovaRegular/fonts__proximaNovaRegular.module.css'
+import './../../common.blocks/container/container.css'
+import back1 from './../../common.blocks/slider/img/Ellipse12.png'
 
 class Main extends React.Component {
     constructor(props) {
@@ -9,8 +13,28 @@ class Main extends React.Component {
 
     render() {
         return (
-            <div className={'page fonts__proximaNovaRegular'}>
-                Main page
+            <div className={`page ${font.fonts__proximaNovaRegular}`}>
+                <Header>
+
+                </Header>
+
+
+                <Slides slides={[
+                    {
+                        header: 'Новинка «Фруктовый десерт»',
+                        back: back1
+                    }, {
+                        header: 'Новинка «Фруктовый десерт»',
+                        back: back1
+                    }, {
+                        header: 'Новинка «Фруктовый десерт»',
+                        back: back1
+                    }, {
+                        header: 'Новинка «Фруктовый десерт»',
+                        back: back1
+                    },]}>
+
+                </Slides>
             </div>
         )
     }
