e01796f7ca9c69d66b3ff7a88532069cb6536579
shootingersNew <lebedevseergey@gmail.com>
2020-04-23 17:29:16 +0300

    added non-complete partners block(need align logos)

---
+++ src/common.blocks/partners/Partners.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './../svgfont/svgfont.css'
+import './partners.css'
+
+export default function Partners() {
+    return (
+        <div className="partners">
+            <div className="container partners__container">
+                <span className="icon-svg__compaqlogo"></span>
+                <span className="icon-svg__verizonlogo"></span>
+                <span className="icon-svg__citibanklogo"></span>
+                <span className="icon-svg__worldplaylogo"></span>
+                <span className="icon-svg__sandisklogo"></span>
+            </div>
+        </div>
+    )
+}
