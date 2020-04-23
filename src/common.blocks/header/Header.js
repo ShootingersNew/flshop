786da14cc5deabc5c020e33d3f736d0e2897753c
shootingersNew <lebedevseergey@gmail.com>
2020-04-23 17:28:30 +0300

    fixed import fonts

--- src/common.blocks/header/Header.js
+++ src/common.blocks/header/Header.js
@@ -3,8 +3,8 @@ import UserNav from "../userNav/UserNav";
 import Logo from "../logo/Logo";
 import './../../common.blocks/svgfont/svgfont.css'
 import './../../common.blocks/catalog/catalog.css'
-import font from './../../common.blocks/fonts/__proximaNovaBold/fonts__proximaNovaBold.module.css'
-import fontReg from './../../common.blocks/fonts/__proximaNovaRegular/fonts__proximaNovaRegular.module.css'
+import '../fonts/__proximaNovaBold/fonts__proximaNovaBold.css'
+import '../fonts/__proximaNovaRegular/fonts__proximaNovaRegular.css'
 import './header.css'
 
 export default function Header() {
@@ -18,12 +18,12 @@ export default function Header() {
                         Каталог
                     </div>
                     <div className="header__telephoneContact">
-                        <a href="tel:+79673110351" className={font.fonts__proximaNovaBold}>8 967 311 03 51 </a>
+                        <a href="tel:+79673110351" className={'fonts__proximaNovaBold'}>8 967 311 03 51 </a>
                         <span>c 8 до 20 ежедневно</span>
                         <a href="#callme" className="header__callPopupLink">Обратный звонок</a>
                     </div>
                     {/*линк для того, чтобы по href найти и отобразить блок(компонент) с соответвующим айди*/}
-                    <input className={`header__search ${fontReg.fonts__proximaNovaRegular}`}
+                    <input className={`header__search fonts__proximaNovaBold`}
                            placeholder={'Поиск по товарам'} type="text"/>
                     <UserNav class={'header__userNav'} itemsIn={3} itemsPrice={'5 608р'}/>
                 </div>
