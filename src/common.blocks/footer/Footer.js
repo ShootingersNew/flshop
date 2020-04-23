21ce3f7f1bded87b0fca354ff165f6a901e91198
shootingersNew <lebedevseergey@gmail.com>
2020-04-23 17:21:04 +0300

    added footer

---
+++ src/common.blocks/footer/Footer.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import './footer.css'
+import './../fonts/__proximaNovaBold/fonts__proximaNovaBold.css'
+import paypal from './img/paypal.svg'
+import yandex from './img/yandex.svg'
+import webmoney from './img/webmoney.svg'
+import visa from './img/visa.svg'
+
+export default function Footer(props) {
+    return (
+        <footer className={'footer'}>
+            <div className="footer__container container">
+                <nav className="footer__nav">
+                    {
+                        props.navs.map((nav) => {
+                            return (
+                                <ul className="footer__navList">
+                                    {
+                                        nav.li.map((li) => {
+                                            return (
+                                                <li key={li.id} className="footer__navLi">
+                                                    <Link to={li.url}>{li.name}</Link>
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            )
+                        })
+                    }
+                </nav>
+                <div className="footer__other">
+                    <div className="footer__paymentLogos">
+                        <div className="footer__paymentLogo footer__paymentLogo_paypal"
+                             style={{backgroundImage: 'url(' + paypal + ')'}}></div>
+                        <div className="footer__paymentLogo footer__paymentLogo_webmoney"
+                             style={{backgroundImage: 'url(' + webmoney + ')'}}></div>
+                        <div className="footer__paymentLogo footer__paymentLogo_yandex"
+                             style={{backgroundImage: 'url(' + yandex + ')'}}></div>
+                        <div className="footer__paymentLogo footer__paymentLogo_visa"
+                             style={{backgroundImage: 'url(' + visa + ')'}}></div>
+                    </div>
+                    <div className="footer__socials">
+                        <a href="#" className="footer__socialLink">
+                            <span className="footer__socialLogo icon-svg__twitter"></span>
+                        </a>
+                        <a href="#" className="footer__socialLink">
+                            <span className="footer__socialLogo icon-svg__vk"></span>
+                        </a>
+                        <a href="#" className="footer__socialLink">
+                            <span className="footer__socialLogo icon-svg__viber"></span>
+                        </a>
+
+                    </div>
+                    <div className="footer__copyrights">
+                        © 2009-2020 Интернет магазин
+                        цветов и подарков janosy.ru
+                        Все права защищены
+                    </div>
+                </div>
+                <div className="footer__contacts">
+                    <a className={"footer__telephone fonts__proximaNovaBold"} href="tel: 89673110351">8 967 311 03
+                        51</a>
+                    <a className={'link footer__mail'} href="mailto:info@flowers.ru">info@flowers.ru</a>
+                    <address className={'footer__address'}>
+                        <span>ТЦ «Цветочный»</span><br/>
+                        Г. Воронеж,<br/>
+                        ул. Цветочная, д. 5
+                    </address>
+                </div>
+            </div>
+        </footer>
+    )
+}
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -3,6 +3,11 @@ import Header from "../../common.blocks/header/Header";
 import Slides from "../../common.blocks/slider/Slider";
 import Banner from "../../common.blocks/banner/Banner";
 import Showcase from "../../common.blocks/showcase/Showcase";
+import OrderControl from "../../common.blocks/orderControl/OrderControl";
+import Subscribe from "../../common.blocks/subscribe/Subscribe";
+import Seo from "../../common.blocks/seo/seo";
+import Partners from "../../common.blocks/partners/Partners";
+import Footer from "../../common.blocks/footer/Footer";
 import './../../common.blocks/page/page.css'
 import '../../common.blocks/fonts/__proximaNovaRegular/fonts__proximaNovaRegular.css'
 import './../../common.blocks/container/container.css'
@@ -130,7 +135,39 @@ class Main extends React.Component {
                         percents: '-32%'
                     },
                 ]}/>
-
+                <OrderControl/>
+                <Subscribe/>
+                <Seo/>
+                <Partners/>
+                <Footer navs={[
+                    {
+                        id: 1, li:
+                            [{id: 1, name: 'Корпоративным клиентам', url: '#'}, {
+                                id: 2,
+                                name: 'Конфеденциальность',
+                                url: '#'
+                            }, {id: 3, name: 'Публичная оферта', url: '#'}, {
+                                id: 4,
+                                name: 'Услуги и сервисы',
+                                url: '#'
+                            }, {id: 5, name: 'Помощь', url: '#'}]
+                    }
+                    , {
+                        id: 2, li:
+                            [{id: 2, name: 'О магазине', url: '#'}, {id: 2, name: 'Франшиза', url: '#'}, {
+                                id: 3,
+                                name: 'Доставка',
+                                url: '#'
+                            }, {id: 4, name: 'Гарантии', url: '#'}, {id: 5, name: 'Оплата', url: '#'}]
+                    }
+                    , {
+                        id: 1, li:
+                            [{id: 3, name: 'Карта сайта', url: '#'}, {id: 2, name: 'Оптовикам', url: '#'}, {
+                                id: 3,
+                                name: 'Мы на ТВ',
+                                url: '#'
+                            }, {id: 4, name: 'Карьера', url: '#'}, {id: 5, name: 'Блог', url: '#'}]
+                    }]}/>
             </div>
         )
     }
