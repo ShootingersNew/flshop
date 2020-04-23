2676ecd77512e9041bfc54982c9fa94ff1040be0
shootingersNew <lebedevseergey@gmail.com>
2020-04-23 17:31:14 +0300

    add non-complete subscribe block (need to style inputs)

---
+++ src/common.blocks/subscribe/Subscribe.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './subscribe.css'
+
+export default function Subscribe() {
+    return (
+        <div className="subscribe">
+            <div className="container subscribe__container">
+                <h3 className="subscribe__header fonts__proximaNovaBold">Еженедельные скидки и акции</h3>
+                <form className={'subscribe__form'}>
+                    <input className={'subscribe__input'} type="text" placeholder={'E-mail'}/>
+                    <button className={'subscribe__submit'}>Подписаться</button>
+                </form>
+            </div>
+        </div>
+    )
+}
