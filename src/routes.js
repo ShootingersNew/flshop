c6fed5b7c405204ae68b213430d504de23eedeea
shootingersNew <lebedevseergey@gmail.com>
2020-04-24 17:33:10 +0300

    change Route for footer for better readability

--- src/routes.js
+++ src/routes.js
@@ -16,35 +16,37 @@ export default function Routes() {
 
                     </Main>
                 </Route>
-                <Route children={<Footer navs={[
-                    {
-                        id: 1, li:
-                            [{id: 1, name: 'Корпоративным клиентам', url: '#'}, {
-                                id: 2,
-                                name: 'Конфеденциальность',
-                                url: '#'
-                            }, {id: 3, name: 'Публичная оферта', url: '#'}, {
-                                id: 4,
-                                name: 'Услуги и сервисы',
-                                url: '#'
-                            }, {id: 5, name: 'Помощь', url: '#'}]
-                    }
-                    , {
-                        id: 2, li:
-                            [{id: 2, name: 'О магазине', url: '#'}, {id: 2, name: 'Франшиза', url: '#'}, {
-                                id: 3,
-                                name: 'Доставка',
-                                url: '#'
-                            }, {id: 4, name: 'Гарантии', url: '#'}, {id: 5, name: 'Оплата', url: '#'}]
-                    }
-                    , {
-                        id: 1, li:
-                            [{id: 3, name: 'Карта сайта', url: '#'}, {id: 2, name: 'Оптовикам', url: '#'}, {
-                                id: 3,
-                                name: 'Мы на ТВ',
-                                url: '#'
-                            }, {id: 4, name: 'Карьера', url: '#'}, {id: 5, name: 'Блог', url: '#'}]
-                    }]}/>}/>
+                <Route>
+                    <Footer navs={[
+                        {
+                            id: 1, li:
+                                [{id: 1, name: 'Корпоративным клиентам', url: '#'}, {
+                                    id: 2,
+                                    name: 'Конфеденциальность',
+                                    url: '#'
+                                }, {id: 3, name: 'Публичная оферта', url: '#'}, {
+                                    id: 4,
+                                    name: 'Услуги и сервисы',
+                                    url: '#'
+                                }, {id: 5, name: 'Помощь', url: '#'}]
+                        }
+                        , {
+                            id: 2, li:
+                                [{id: 2, name: 'О магазине', url: '#'}, {id: 2, name: 'Франшиза', url: '#'}, {
+                                    id: 3,
+                                    name: 'Доставка',
+                                    url: '#'
+                                }, {id: 4, name: 'Гарантии', url: '#'}, {id: 5, name: 'Оплата', url: '#'}]
+                        }
+                        , {
+                            id: 1, li:
+                                [{id: 3, name: 'Карта сайта', url: '#'}, {id: 2, name: 'Оптовикам', url: '#'}, {
+                                    id: 3,
+                                    name: 'Мы на ТВ',
+                                    url: '#'
+                                }, {id: 4, name: 'Карьера', url: '#'}, {id: 5, name: 'Блог', url: '#'}]
+                        }]}/>
+                </Route>
             </App>
         </Router>
     )
