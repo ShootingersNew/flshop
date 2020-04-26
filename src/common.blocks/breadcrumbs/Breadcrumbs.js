93ead6d46cdabcd0d77b8da952c23c0cb6cfa131
shootingersNew <lebedevseergey@gmail.com>
2020-04-26 16:33:12 +0300

    add breadcrumbs

---
+++ src/common.blocks/breadcrumbs/Breadcrumbs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './breadcrumbs.css'
+
+export default function Breadcrumbs() {
+    return (
+        <div className="breadcrumbs">
+            <a className={'breadcrumbs__link'} href="/">Главная</a><span className={'breadcrumbs__separator'}> / </span>
+            <a className={'breadcrumbs__link'} href="#">Каталог</a><span className={'breadcrumbs__separator'}> / </span>
+            <a className={'breadcrumbs__link'} href="#">Розы</a><span className={'breadcrumbs__separator'}> / </span>
+            <a className={'breadcrumbs__active'} href="#">Музыка цветов</a>
+        </div>
+    )
+}
