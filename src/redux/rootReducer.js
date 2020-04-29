8639e1fd93dfa52bf70738ab92768f7d32e50b56
shootingersNew <lebedevseergey@gmail.com>
2020-04-29 09:46:53 +0300

    connect cartReducer with store through rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
-import {goodsReducer} from "./goodsReducer";
+import goodsReducer from "./goodsReducer";
+import cartReducer from "./cartReducer/cartReducer";
 
 export const rootReducer = combineReducers({
-    goods: goodsReducer
+    goods: goodsReducer,
+    cart: cartReducer,
 });
