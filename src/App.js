de58d85d27579cbd837181979ea9398284742047
shootingersNew <lebedevseergey@gmail.com>
2020-05-12 17:21:25 +0300

    refactored app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Header from "./common.blocks/header/Header";
-import Footer from "./common.blocks/footer/Footer";
 
 function App(props) {
   return (
       <div className={'page fonts__proximaNovaRegular'}>
         {props.children}
-
       </div>
   );
 }
