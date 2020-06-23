2aca63ee2c43d47204e7b8e20cfdd7ca6f6fea86
shootingersNew <lebedevseergey@gmail.com>
2020-06-23 15:15:08 +0300

    changed 1) added proptypes 2) refactored imports 3) added conditional rendering ( depends of props.controlled )

--- src/common.blocks/checkbox/Checkbox.js
+++ src/common.blocks/checkbox/Checkbox.js
@@ -1,17 +1,45 @@
-import React from "react";
-import './checkbox.css';
+//libs
+import React from "react"
+import PropTypes from 'prop-types'
+//styles
+import './checkbox.css'
 
+Checkbox.propTypes = {
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    checked: PropTypes.bool,
+    defaultChecked: PropTypes.bool,
+    ref: PropTypes.func,
+    controlled: PropTypes.bool
+
+};
 export default function Checkbox(props) {
     return (
-        <label className={'checkbox ' + props.className + ' checkbox_checked_' + props.checked}>
-        <span className="checkbox__box">
-             <input
-                 className={'checkbox__true'}
-                 checked={props.checked}
-                 onChange={() => props.onChange(!props.checked)}
-                 type="checkbox"
-             />
-        </span>
+        <label className={'checkbox ' + props.className}>
+            {props.controlled ? <input
+                    className={'checkbox__true'}
+                    type={'checkbox'}
+                    data-inputtype={props.type}
+                    name={props.name}
+                    value={props.value}
+                    onChange={props.onChange ? props.onChange : undefined}
+                    checked={props.checked ? props.checked : null}
+                    ref={props.register ? props.register : null}
+                />
+                :
+                <input
+                    className={'checkbox__true'}
+                    type={'checkbox'}
+                    data-inputtype={props.type}
+                    name={props.name}
+                    value={props.value}
+                    onChange={props.onChange ? props.onChange : undefined}
+                    defaultChecked={props.defaultChecked ? props.defaultChecked : null}
+                    ref={props.register ? props.register : null}
+                />}
+            <span className="checkbox__box"/>
+
         </label>
     )
 }
