54ee8bba5cb36e76b9dd2697c51935363a20e993
shootingersNew <lebedevseergey@gmail.com>
2020-06-23 15:48:20 +0300

    1) removed rendering rounds of rating, instead of this rating displaying now as mark (4.3 etc) 2) link 'add to card' changed to Button 3) added proptypes 4) refactored imports

--- src/common.blocks/flower/Flower.js
+++ src/common.blocks/flower/Flower.js
@@ -1,75 +1,59 @@
-import React from "react";
+//libs
+import React from "react"
+import PropTypes from 'prop-types'
+//comps
+import Composition from "./composition/Composition"
+import Button from "../button/Button"
+//styles || utils
 import './../link/link.css'
 import './../fonts/__proximaNovaBold/fonts__proximaNovaBold.css'
-import Flower__compositionLi from "./flower__compositionLi/Flower__compositionLi";
-import {regExpPrice} from "../../config/utils";
-import b from './../button/button.module.css'
 import './flower.css'
+import star from './img/star.svg'
+import {regExpPrice} from "../../config/utils"
 
-
+Flower.propTypes = {
+    item: PropTypes.object.isRequired
+};
 export default function Flower(props) {
-    //рендерю точки рейтинга
-    function renderMark(mark) {
-        const markContent = [];
-        for (let i = 0; i < 5; i++) {
-            if (i < mark) {
-                markContent.push(<span className="flower__markRound flower__markRound_active"></span>)
-            } else {
-                markContent.push(<span className="flower__markRound"></span>)
-            }
-        }
-        return markContent
-    }
-
     return (
         <React.Fragment>
             {props.item ?
-
-                <article className={'flower'}>
-                    <div style={{backgroundImage: "url(" + props.item.src + ")"}} className={'flower__image'}>Тут
-                        изображение для цветка, который вы выбрали.
-                    </div>
-                    <div className="flower__control">
-                        <div className="flower__info">
-                            <h1 className="flower__header">{props.item.name}</h1>
-                            <div className="flower__vendorCode">
-                                Артикул<span>{props.item.vendorCode}</span>
+                <div className="flower__wrapper">
+                    <article className={'flower'}>
+                        <div style={{backgroundImage: "url(" + props.item.src + ")"}} className={'flower__image'}/>
+                        <div className="flower__control">
+                            <div className="flower__info">
+                                <h1 className="flower__header">{props.item.name}</h1>
+                                <div className="flower__vendorCode">
+                                    Артикул<span>{props.item.vendorCode}</span>
+                                </div>
+                                <div className="flower__mark">
+                                    <img src={star} alt="star"/> {props.item.mark}
+                                </div>
+                                <a href="#" className="flower__reviewsLink link">8 отзывов</a>
                             </div>
-                            <div className="flower__mark">
-                                {
-                                    renderMark(props.item.mark)
-                                }
-                            </div>
-                            <a href="#" className="flower__reviewsLink link">8 отзывов</a>
-                        </div>
-                        <div className="flower__price">
+                            <div className="flower__price">
                             <span
                                 className="flower__actualPrice fonts__proximaNovaBold">{regExpPrice(props.item.price)}р</span>
-                            {props.sale ?
-                                //todo чекнуть как будет выглядеть со скидкой
-                                <div className="flower__sale">
-                                    <span className="flower__salePrice">4 500р</span>
-                                    <span className="flower__salePercents"><span>-55%</span></span>
-                                </div> : null
-                            }
-                        </div>
-                        <div className="flower__buttons">
-                            <a className={b.button + ' flower__inCart'} href="#">В корзину</a>
-                            <a className={'flower__oneClickBuy fonts__proximaNovaBold'} href="#">Купить в один клик</a>
-                        </div>
-                        <footer className="flower__composition">
-                            <div className="flower__compositionHeader">Композиция</div>
-                            {/*href указывает на название цветка в данных, получаемых из БД*/}
-                            <ul className="flower__compositionList">
-                                {props.item.composition.map((item) => {
-                                    return <Flower__compositionLi item={item} class={'flower__compositionPreloader'}/>
-                                })}
+                                {props.item.sale ?
 
-                            </ul>
+                                    <div className="flower__sale">
+                                        <span className="flower__salePrice">{props.item.salePrice}р</span>
+                                        <span className="flower__salePercents"><span>{props.item.percents}</span></span>
+                                    </div> : null
+                                }
+                            </div>
+                            <div className="flower__buttons">
+                                <Button className={' flower__inCart'}>В корзину</Button>
+                                <a className={'flower__oneClickBuy fonts__proximaNovaBold'} href="#">Купить в один
+                                    клик</a>
+                            </div>
+                            <Composition composition={props.item.composition}/>
+                        </div>
+                    </article>
 
-                        </footer>
-                    </div>
-                </article>
+                </div>
+
                 :
                 null
                 //    todo сделать страницу error
