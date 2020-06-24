bf3840894af6b258840b5ea882003677ec8755ba
shootingersNew <lebedevseergey@gmail.com>
2020-06-24 14:18:31 +0300

    refactored imports, added proptypes, classnames library, deleted h2 header

--- src/common.blocks/verticalBanner/VerticalBanner.js
+++ src/common.blocks/verticalBanner/VerticalBanner.js
@@ -1,15 +1,25 @@
+//libs
 import React from "react";
 import {Link} from "react-router-dom";
+import cn from "classnames";
+import PropTypes from 'prop-types';
+//styles
 import './verticalBanner.css'
 import '../fonts/__proximaNovaBlack/fonts__proximaNovaBlack.css'
 
+VerticalBanner.propTypes = {
+    banner: PropTypes.object.isRequired,
+    className: PropTypes.string,
+    type: PropTypes.string
+};
 export default function VerticalBanner(props) {
+    let className = cn({
+        'verticalBanner fonts__proximaNovaBlack': true,
+        [props.className]: props.className,
+        [props.type]: props.type,
+    });
     return (
         <Link to={props.banner.url} style={{backgroundImage: 'url(' + props.banner.bg + ')'}}
-              className={'verticalBanner fonts__proximaNovaBlack ' + props.className}>
-            <h2 className={'verticalBanner__header'}>
-                {props.banner.header}
-            </h2>
-        </Link>
+              className={className}/>
     )
 }
