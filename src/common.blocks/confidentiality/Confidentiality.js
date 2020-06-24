154646aa31b55969beeaea77748c1693bb36ce7f
shootingersNew <lebedevseergey@gmail.com>
2020-06-24 12:34:31 +0300

    imports refactored, added classnames library, added register && default checked props, removed Onchange and checked props

--- src/common.blocks/confidentiality/Confidentiality.js
+++ src/common.blocks/confidentiality/Confidentiality.js
@@ -1,16 +1,25 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import Checkbox from "../checkbox/Checkbox";
+//libs
+import React from "react"
+import {Link} from "react-router-dom"
+import cn from 'classnames'
+//comps
+import Checkbox from "../checkbox/Checkbox"
+//styles
 import './confidentiality.css'
 import './../link/link.css'
 
 export default function Confidentiality(props) {
+    let classname = cn({
+        'confidentiality': true,
+        [props.className]: props.className
+    });
     return (
-        <div className={'confidentiality ' + props.className}>
+        <div className={classname}>
             <Checkbox
+                name={'confidentiality'}
                 className={'confidentiality__checkbox'}
-                checked={props.checked}
-                onChange={props.onChange}
+                register={props.register ? props.register : null}
+                defaultChecked={props.defaultChecked}
             />
             <div className="confidentiality__text">
                 Согласен(на) с условиями&ensp;
