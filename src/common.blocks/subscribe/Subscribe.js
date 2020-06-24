dcc0111df96738cc49c6195763172fbdfbcf0210
shootingersNew <lebedevseergey@gmail.com>
2020-06-24 14:04:25 +0300

    tag form replaced by component Form, refactored imports

--- src/common.blocks/subscribe/Subscribe.js
+++ src/common.blocks/subscribe/Subscribe.js
@@ -1,15 +1,36 @@
-import React from "react";
+//libs
+import React from "react"
+//comps
+import Input from "../input/Input"
+import Button from "../button/Button"
+import Form from "../form/Form"
+//utils
+import {emailRegexp} from "../../config/config"
+//styles
 import './subscribe.css'
+import '../fonts/__proximaNovaBold/fonts__proximaNovaBold.css'
 
 export default function Subscribe() {
     return (
         <div className="subscribe">
             <div className="subscribe__container">
                 <h3 className="subscribe__header fonts__proximaNovaBold">Еженедельные скидки и акции</h3>
-                <form className={'subscribe__form'}>
-                    <input className={'subscribe__input'} type="text" placeholder={'E-mail'}/>
-                    <button className={'subscribe__submit'}>Подписаться</button>
-                </form>
+                <Form render={({register}, isInputValid) => (
+                    <div className="subscribe__input-wrapper">
+                        <Input
+                            register={register({
+                                pattern: emailRegexp
+                            })}
+                            isValid={isInputValid('mail')}
+                            name={'mail'}
+                            className={'subscribe__input'}
+                            placeholder={'E-mail'}
+                        />
+                        <Button className={'fonts__proximaNovaBold'} type={'input'}>Подписаться</Button>
+                    </div>
+                )}
+                >
+                </Form>
             </div>
         </div>
     )
