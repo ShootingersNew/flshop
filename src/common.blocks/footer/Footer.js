008b5a103417a4c643cc691769092b8f926b0671
shootingersNew <lebedevseergey@gmail.com>
2020-06-24 12:12:07 +0300

    refactored imports, proptypes added

--- src/common.blocks/footer/Footer.js
+++ src/common.blocks/footer/Footer.js
@@ -1,13 +1,20 @@
-import React from "react";
-import {Link} from "react-router-dom";
+//libs
+import React from "react"
+import {Link} from "react-router-dom"
+import PropTypes from 'prop-types'
+//comps
+import Container from "../container/Container"
+//styles
 import './footer.css'
 import './../fonts/__proximaNovaBold/fonts__proximaNovaBold.css'
-import Container from "../container/Container";
 import paypal from './img/paypal.svg'
 import yandex from './img/yandex.svg'
 import webmoney from './img/webmoney.svg'
 import visa from './img/visa.svg'
 
+Footer.propTypes = {
+    navs: PropTypes.array.isRequired
+};
 export default function Footer(props) {
     return (
         <footer className={'footer'}>
@@ -34,23 +41,23 @@ export default function Footer(props) {
                 <div className="footer__other">
                     <div className="footer__paymentLogos">
                         <div className="footer__paymentLogo footer__paymentLogo_paypal"
-                             style={{backgroundImage: 'url(' + paypal + ')'}}></div>
+                             style={{backgroundImage: 'url(' + paypal + ')'}}/>
                         <div className="footer__paymentLogo footer__paymentLogo_webmoney"
-                             style={{backgroundImage: 'url(' + webmoney + ')'}}></div>
+                             style={{backgroundImage: 'url(' + webmoney + ')'}}/>
                         <div className="footer__paymentLogo footer__paymentLogo_yandex"
-                             style={{backgroundImage: 'url(' + yandex + ')'}}></div>
+                             style={{backgroundImage: 'url(' + yandex + ')'}}/>
                         <div className="footer__paymentLogo footer__paymentLogo_visa"
-                             style={{backgroundImage: 'url(' + visa + ')'}}></div>
+                             style={{backgroundImage: 'url(' + visa + ')'}}/>
                     </div>
                     <div className="footer__socials">
                         <a href="#" className="footer__socialLink">
-                            <span className="footer__socialLogo icon-svg__twitter"></span>
+                            <span className="footer__socialLogo icon-svg__twitter"/>
                         </a>
                         <a href="#" className="footer__socialLink">
-                            <span className="footer__socialLogo icon-svg__vk"></span>
+                            <span className="footer__socialLogo icon-svg__vk"/>
                         </a>
                         <a href="#" className="footer__socialLink">
-                            <span className="footer__socialLogo icon-svg__viber"></span>
+                            <span className="footer__socialLogo icon-svg__viber"/>
                         </a>
 
                     </div>
