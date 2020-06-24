939a3bc21fe2b33dc996f61d7b1f5cfb4c090ffb
shootingersNew <lebedevseergey@gmail.com>
2020-06-24 12:29:56 +0300

    refactored tags

--- src/common.blocks/partners/Partners.js
+++ src/common.blocks/partners/Partners.js
@@ -6,11 +6,11 @@ export default function Partners() {
     return (
         <div className="partners">
             <div className="partners__container">
-                <span className="icon-svg__compaqlogo"></span>
-                <span className="icon-svg__verizonlogo"></span>
-                <span className="icon-svg__citibanklogo"></span>
-                <span className="icon-svg__worldplaylogo"></span>
-                <span className="icon-svg__sandisklogo"></span>
+                <span className="icon-svg__compaqlogo"/>
+                <span className="icon-svg__verizonlogo"/>
+                <span className="icon-svg__citibanklogo"/>
+                <span className="icon-svg__worldplaylogo"/>
+                <span className="icon-svg__sandisklogo"/>
             </div>
         </div>
     )
