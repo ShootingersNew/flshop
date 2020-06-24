c5509c7ce7e7471df5618a8e89ff5467b598e976
shootingersNew <lebedevseergey@gmail.com>
2020-06-24 14:54:17 +0300

    added additional items

---
+++ src/common.blocks/additionalItems/AdditionalItems.js
@@ -0,0 +1,24 @@
+//libs
+import React from "react";
+import PropTypes from 'prop-types'
+//comps
+import Bouquet from "../bouquet/Bouquet"
+//styles
+import './additionalItems.css'
+
+AdditionalItems.propTypes = {
+    addItems: PropTypes.array.isRequired
+};
+export default function AdditionalItems(props) {
+    return (
+        <div className="additionalItems">
+            <div className="additionalItems__header">C этим товаром также приобретают</div>
+            <div className="additionalItems__inner">
+                {props.addItems && props.addItems.map((item) => {
+                    return (<Bouquet item={item}/>)
+                })
+                }
+            </div>
+        </div>
+    )
+}
