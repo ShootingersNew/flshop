16bf0313195b8a2e5619e83a2e533565cc66ba11
shootingersNew <lebedevseergey@gmail.com>
2020-07-01 19:09:05 +0300

    partners fixed position of logos

--- src/common.blocks/partners/Partners.js
+++ src/common.blocks/partners/Partners.js
@@ -1,4 +1,5 @@
 import React from "react";
+//styles
 import './../svgfont/svgfont.css'
 import './partners.css'
 
@@ -6,11 +7,11 @@ export default function Partners() {
     return (
         <div className="partners">
             <div className="partners__container">
-                <span className="icon-svg__compaqlogo"/>
-                <span className="icon-svg__verizonlogo"/>
-                <span className="icon-svg__citibanklogo"/>
-                <span className="icon-svg__worldplaylogo"/>
-                <span className="icon-svg__sandisklogo"/>
+                <span className="partners__logo icon-svg__compaqlogo"/>
+                <span className="partners__logo partners__verizon icon-svg__verizonlogo"/>
+                <span className="partners__logo partners__citibank icon-svg__citibanklogo"/>
+                <span className="partners__logo partners__worldplay icon-svg__worldplaylogo"/>
+                <span className="partners__logo partners__sandisk icon-svg__sandisklogo"/>
             </div>
         </div>
     )
