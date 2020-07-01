42f7ae5e08e474d5e3a1104bed573afdc8af2f49
shootingersNew <lebedevseergey@gmail.com>
2020-07-01 17:44:02 +0300

    Fieldset.js added 1)new flowers checkboxes 2) added default open fieldset 3) proptypes some classes removed

--- src/common.blocks/filters/Filters.js
+++ src/common.blocks/filters/Filters.js
@@ -56,20 +56,21 @@ export default class Filters extends React.Component {
         }
     };
 
+
     render() {
         const {filtersState, setFilters} = this.props;
         return (
             <form className={'filters'}>
-                <h4 className={'filters__mainHeader'}>Фильтры</h4>
-                <div className="filters__singleFilter">
-                    <h5 className="filters__header">Цена</h5>
+                <h4 className={'filters__main-header fonts__proximaNovaSemibold'}>Фильтры</h4>
+                <div className="filters__single-filter">
+                    <h5 className="filters__header fonts__proximaNovaBold">Цена</h5>
 
                     <PriceFilter
                         changeHandler={this.setPrice}
                     />
 
                 </div>
-                <div className="filters__singleFilter">
+                <div className="filters__single-filter">
                     <label className={'filters__label'}>
                         <Checkbox
                             name={'showWithSale'}
@@ -90,6 +91,7 @@ export default class Filters extends React.Component {
                         type={'radio'}
                         header={'Количество цветов'}
                         filters={filtersState.checkboxes}
+                        countOpenedFilters={this.props.countOpenedFilters}
                     />
 
                     <Fieldset
@@ -97,6 +99,7 @@ export default class Filters extends React.Component {
                         type={'checkbox'}
                         header={'Окрас букета'}
                         filters={filtersState.checkboxes}
+                        countOpenedFilters={this.props.countOpenedFilters}
                     />
 
                     <Fieldset
@@ -104,6 +107,7 @@ export default class Filters extends React.Component {
                         type={'radio'}
                         filters={filtersState.checkboxes}
                         header={'Сортировать'}
+                        countOpenedFilters={this.props.countOpenedFilters}
                     />
 
                     <Fieldset
@@ -111,6 +115,8 @@ export default class Filters extends React.Component {
                         type={'radio'}
                         filters={filtersState.checkboxes}
                         header={'Цветы'}
+                        isDefaultOpened={true}
+                        countOpenedFilters={this.props.countOpenedFilters}
                     />
 
                     <Fieldset
@@ -118,10 +124,12 @@ export default class Filters extends React.Component {
                         type={'radio'}
                         filters={filtersState.checkboxes}
                         header={'Кому'}
+                        countOpenedFilters={this.props.countOpenedFilters}
                     />
 
 
-                    <Button mod={'_filter button_transparent '} onClick={this.props.uncheckAllCheckboxes}>
+                    <Button mod={'filter button_transparent'} className={'filters__button'}
+                            onClick={this.props.uncheckAllCheckboxes}>
                         Очистить фильтры
                     </Button>
 
--- src/common.blocks/filters/fieldset/Fieldset.js
+++ src/common.blocks/filters/fieldset/Fieldset.js
@@ -1,4 +1,7 @@
-import React, {useState} from "react";
+//libs
+import React, {useEffect, useState} from "react";
+import PropTypes from 'prop-types'
+//comps
 import Checkbox from "../../checkbox/Checkbox";
 
 const Fieldset = (props) => {
@@ -29,6 +32,11 @@ const Fieldset = (props) => {
             'Тюльпаны',
             'Пионы',
             'Ирисы',
+            'Лиллии',
+            'Розы',
+            'Гвоздики',
+            'Ромашки',
+            'Орхидеи'
         ],
         for: [
             'Маме',
@@ -40,21 +48,33 @@ const Fieldset = (props) => {
     const [isOpened, setIsOpened] = useState(false);
     const [isChanged, setIsChanged] = useState(false);
 
+    useEffect(() => {
+        if (props.isDefaultOpened) {
+            setIsOpened(props.isDefaultOpened)
+        }
+    }, [props.isDefaultOpened]);
+
     function mapCheckboxes(name, type) {
-        return addFilterArr[name].map((checkbox) => {
+        return addFilterArr[name].map((checkbox, idx) => {
 
             return (
-                <label className={'filters__label'}>
+                <label key={idx} className={'filters__label'}>
 
                     {
                         type === 'radio' ?
-                            <Checkbox controlled={true} type={type}
-                                      checked={props.hasOwnProperty('filters') && props.filters[name] === checkbox}
-                                      value={checkbox} name={name}/>
+                            <Checkbox
+                                controlled={true} type={type}
+                                checked={props.hasOwnProperty('filters') && props.filters[name] === checkbox}
+                                value={checkbox} name={name}
+                                className={'checkbox_red'}
+                            />
                             :
-                            <Checkbox type={type}
-                                      checked={props.hasOwnProperty('filters') && props.filters[name] && props.filters[name].includes(checkbox)}
-                                      value={checkbox} name={name}/>
+                            <Checkbox
+                                type={type}
+                                checked={props.hasOwnProperty('filters') && props.filters[name] && props.filters[name].includes(checkbox)}
+                                value={checkbox} name={name}
+                                className={'filters__checkbox checkbox_small-arrow'}
+                            />
                     }
                     <span className="filters__text"> {checkbox}</span>
                 </label>
@@ -62,12 +82,16 @@ const Fieldset = (props) => {
         })
     }
 
+    function openHandler() {
+        setIsOpened(!isOpened);
+        isOpened ? props.countOpenedFilters(-1) : props.countOpenedFilters(1);
+        setIsChanged(true);
+    }
 
     return (
         <fieldset className={'filters__fieldset filters__fieldset_opened_' + isOpened}>
             <legend onClick={() => {
-                setIsOpened(!isOpened);
-                setIsChanged(true)
+                openHandler()
             }} className={'filters__legend fonts__proximaNovaBold'}>
                 <span
                     className={"icon-svg__arrow filters__tick filters__tick_animationReady_" + isChanged}/> {props.header}
@@ -79,5 +103,13 @@ const Fieldset = (props) => {
             </div>
         </fieldset>
     )
+};
+Fieldset.propTypes = {
+    checkboxName: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    filters: PropTypes.object.isRequired,
+    header: PropTypes.string.isRequired,
+    isDefaultOpened: PropTypes.bool,
+    countOpenedFilters: PropTypes.func.isRequired
 };
 export default Fieldset
