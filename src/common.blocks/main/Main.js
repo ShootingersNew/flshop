3c2736178cd9dc51108da1d8261616415887ff73
shootingersNew <lebedevseergey@gmail.com>
2020-07-01 18:54:57 +0300

    Main.js added template jsx and styles

--- src/common.blocks/main/Main.js
+++ src/common.blocks/main/Main.js
@@ -1,4 +1,9 @@
+//libs
 import React from "react";
+//comps
+import Container from "../container/Container";
+import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
+//styles
 import './main.css'
 
 export default function Main(props) {
@@ -6,8 +11,52 @@ export default function Main(props) {
 
     return (
         <main className={'main ' + className}>
-            {
-                props.children
+            {props.container ?
+                <Container className={'main__container'}>
+                    {
+                        props.breadcrumbsArray && <Breadcrumbs items={props.breadcrumbsArray}/>
+                    }
+                    {
+                        props.content ?
+                            <div className="main__flex-inner">
+                                <div className={'main__content'}>
+                                    {
+                                        props.content
+                                    }
+                                </div>
+                                <div className="main__aside">
+                                    {
+                                        props.aside
+                                    }
+                                </div>
+                            </div>
+                            :
+                            props.children
+                    }
+                </Container>
+                :
+                <React.Fragment>
+                    {
+                        props.breadcrumbsArray && <Breadcrumbs items={props.breadcrumbsArray}/>
+                    }
+                    {
+                        props.content ?
+                            <div className="main__flex-inner">
+                                <div className={'main__content'}>
+                                    {
+                                        props.content
+                                    }
+                                </div>
+                                <div className="main__aside">
+                                    {
+                                        props.aside
+                                    }
+                                </div>
+                            </div>
+                            :
+                            props.children
+                    }
+                </React.Fragment>
             }
         </main>
     )
