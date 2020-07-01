a7068301061570e3ac3ba70ffd7ef35d0b3c83e3
shootingersNew <lebedevseergey@gmail.com>
2020-07-01 18:33:07 +0300

    Input.js removed useless styles

--- src/common.blocks/input/Input.js
+++ src/common.blocks/input/Input.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames'
 //styles
 import './input.css'
-import './../fonts/__proximaNovaRegular/fonts__proximaNovaRegular.css'
 
 Input.propTypes = {
     classname: PropTypes.string,
