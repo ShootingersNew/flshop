c284c4f2f436646fe0d781ada0177c8640a83439
shootingersNew <lebedevseergey@gmail.com>
2020-07-01 18:32:44 +0300

    Header.js -1)changed header of Modal fixed link removed useless styles

--- src/common.blocks/header/Header.js
+++ src/common.blocks/header/Header.js
@@ -8,40 +8,34 @@ import Modal from "../modal/Modal"
 import CallModalView from "../modal/views/CallModalView/CallModalView"
 import Search from "../search/Search"
 //styles
-import '../link/link.css'
 import '../svgfont/svgfont.css'
 import '../catalog/catalog.css'
-import '../fonts/__proximaNovaBold/fonts__proximaNovaBold.css'
-import '../fonts/__proximaNovaRegular/fonts__proximaNovaRegular.css'
 import 'simplebar/dist/simplebar.min.css'
 import './header.css'
 
 export default function Header() {
 
     return (
-        <header>
-            <div className="header ">
-                <div className={'header__container container'}>
-                    <Logo/>
-                    <Link to={'./catalog'} className="header__catalogButton catalog__button link link_color_inherit ">
-                        <span className={'icon-svg__bar catalog__icon'}/>
-                        Каталог
-                    </Link>
-                    <div className="header__telephoneContact">
-                        <a href="tel:+79673110351" className={'fonts__proximaNovaBold'}>8 967 311 03 51 </a>
-                        <span className={'header__work-schedule'}>c 8 до 20 ежедневно</span>
-                        <Modal
-                            trigger={<span className="header__callPopupLink">Обратный звонок</span>}
-                            header={'Сообщение'}
-                            content={<CallModalView/>}
+        <header className={'header'}>
+            <div className={'header__container container'}>
+                <Logo/>
+                <Link to={'/catalog'} className="header__catalogButton catalog__button link link_color_inherit ">
+                    <span className={'icon-svg__bar catalog__icon'}/>
+                    Каталог
+                </Link>
+                <div className="header__telephoneContact">
+                    <a href="tel:+79673110351" className={'fonts__proximaNovaBold'}>8 967 311 03 51 </a>
+                    <span className={'header__work-schedule'}>c 8 до 20 ежедневно</span>
+                    <Modal
+                        trigger={<span className="header__callPopupLink">Обратный звонок</span>}
+                        header={'Обратный звонок'}
+                        content={<CallModalView/>}
                         />
                     </div>
                     <Search class={'header__search'}/>
                     {/*линк для того, чтобы по href найти и отобразить блок(компонент) с соответвующим айди*/}
                     <UserNav class={'header__userNav'}/>
                 </div>
-
-            </div>
         </header>
     )
 }
