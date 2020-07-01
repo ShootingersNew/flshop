b7ce4a057a8453c8975886e1a08abc5f6da7927d
shootingersNew <lebedevseergey@gmail.com>
2020-07-01 16:21:53 +0300

    changed in Banner 1)removed useless fonts && button 2)added render props

--- src/common.blocks/banner/Banner.js
+++ src/common.blocks/banner/Banner.js
@@ -1,27 +1,21 @@
 //libs
 import React from "react"
 import PropTypes from 'prop-types'
-//comps
-import Button from "../button/Button"
 //styles
 import './../container/container.css'
 import banner from './banner.module.css'
-import '../fonts/__proximaNovaBold/fonts__proximaNovaBold.css'
 
 Banner.propTypes = {
     bg: PropTypes.string.isRequired,
-    header: PropTypes.string.isRequired,
+    render: PropTypes.element.isRequired
 };
 
 export default function Banner(props) {
     return (
         <article className={banner.banner} style={{backgroundImage: 'url(' + props.bg + ')'}}>
             <div className={'container ' + banner.banner__container}>
-                <div className="banner__inner">
-                    <header className={banner.banner__header + ' fonts__proximaNovaBold'}>
-                        {props.header}
-                    </header>
-                    <Button link={props.linkUrl} className={'banner__link'}>Заказать</Button>
+                <div className={banner.banner__inner}>
+                    {props.render}
                 </div>
             </div>
         </article>
