fb883587c138c44297ba16eb9d101c5d95b7a761
shootingersNew <lebedevseergey@gmail.com>
2020-07-02 16:58:30 +0300

    routes.js- removed useless imports of header & footer & removed 'exact' from all routes except /

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {MainPage} from "./pages/mainPage/MainPage";
 import Goods from "./pages/good/Goods";
-import Header from "./common.blocks/header/Header";
-import Footer from "./common.blocks/footer/Footer";
 import App from "./App";
 import CartPage from "./pages/cartPage/CartPage";
 import ListingPage from "./pages/listingPage/ListingPage";
@@ -20,9 +18,9 @@ export default function Routes() {
                         <Route exact path={"/"}>
                             <MainPage/>
                         </Route>
-                        <Route exact path={'/goods/:type/:idx'} component={Goods}/>
-                        <Route exact path={'/cart'} component={CartPage}/>
-                        <Route exact path={'/catalog'} component={ListingPage}/>
+                        <Route path={'/goods/:type/:idx'} component={Goods}/>
+                        <Route path={'/cart'} component={CartPage}/>
+                        <Route path={'/catalog'} component={ListingPage}/>
                     </Switch>
                 </App>
             </Router>
