072bee5a96d0fcc5d9bfeef933f06ab6c565d039
shootingersNew <lebedevseergey@gmail.com>
2020-07-02 16:54:41 +0300

    MainPage.js  changed Slider on SlickSlider,case Slider comp was Renamed,Banner now renders custom text,changed header in Showcases

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,7 +1,7 @@
 //libs
 import React from "react"
 //comps
-import Slides from "../../common.blocks/slider/Slider"
+import SlickSlider from "../../common.blocks/SlickSlider/SlickSlider"
 import Banner from "../../common.blocks/banner/Banner"
 import Showcase from "../../common.blocks/showcase/Showcase"
 import OrderControl from "../../common.blocks/orderControl/OrderControl"
@@ -12,47 +12,71 @@ import Container from "../../common.blocks/container/Container"
 import Main from "../../common.blocks/main/Main"
 //styles
 import '../../common.blocks/container/container.css'
-import back1 from '../../common.blocks/slider/img/Ellipse12.png'
+import banner from "../../common.blocks/banner/banner.module.css";
+import Button from "../../common.blocks/button/Button";
 
 class MainPage extends React.Component {
     render() {
         return (
             <Main>
                 {/*dots - здесь бэкграунды*/}
-                <Slides
+                <SlickSlider
                     slides={[
                         {
                             header: 'Новинка «Фруктовый десерт»',
-                            back: back1
+                            back: '../../images/slider/1.png'
+                        }, {
+                            header: 'Особенный букет для невесты ',
+                            back: '../../images/slider/2.png'
                         }, {
                             header: 'Новинка «Фруктовый десерт»',
-                            back: back1
+                            back: '../../images/slider/3.png'
                         }, {
                             header: 'Новинка «Фруктовый десерт»',
-                            back: back1
-                        }, {
-                            header: 'Новинка «Фруктовый десерт»',
-                            back: back1
+                            back: '../../images/slider/4.png'
                         },
                     ]}
-                    dots={['', '', '', '']}/>
+                />
                 <Banner
                     key={1}
-                    linkUrl={'#'}
-                    header={'Букет «Женское изящество»'}
-                    bg={'#'}
+                    bg={'../../images/banners/banner1.png'}
+                    render={
+                        <React.Fragment>
+                            <header className={banner.banner__header + ' fonts__proximaNovaBold'}>
+                                Подарки <br/> к заказам на 8 марта
+                            </header>
+                            <Button className={'banner__link'}>Подробнее</Button>
+                        </React.Fragment>
+                    }
                 />
 
                 {/*showcase обернут в hoc withBDComponent, idx - индекс showcase в базе данных*/}
 
-                <Showcase header={'Женственные подарки к 8 марта'} idx={'showcase_1'}/>
+                <Showcase
+                    header={'Женственные подарки к 8 марта'}
+                    idx={'showcase_1'}
+                    showcaseType={'mini'}
+                    counter={6}
+                />
                 <Banner
                     key={2}
-                    linkUrl={'#'}
-                    header={'Букет «Ангел» за 990р'}
-                    bg={'#'}
+                    bg={'../../images/banners/banner2.png'}
+                    render={
+                        <React.Fragment>
+                            <header className={banner.banner__header + ' fonts__proximaNovaBold'}>
+                                Подарки к заказам на 8 марта
+                            </header>
+                            <Button className={'banner__link'}>Заказать</Button>
+                        </React.Fragment>
+                    }
+
                 />
-                <Showcase header={'Летние акции'} idx={'showcase_2'}/>
+                <Showcase
+                    header={'Летние акции'}
+                    idx={'showcase_2'}
+                    showcaseType={'mini'}
+                    counter={11}
+                />
                 <Container>
                     <OrderControl/>
                     <Subscribe/>
