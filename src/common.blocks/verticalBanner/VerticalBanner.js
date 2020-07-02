1de8c09d3c791dd49bbd94782f401a92e20b304a
shootingersNew <lebedevseergey@gmail.com>
2020-07-02 16:56:09 +0300

    VerticalBanner 1)removed header & styles of header 2)props. type now is mod

--- src/common.blocks/verticalBanner/VerticalBanner.js
+++ src/common.blocks/verticalBanner/VerticalBanner.js
@@ -5,7 +5,6 @@ import cn from "classnames";
 import PropTypes from 'prop-types';
 //styles
 import './verticalBanner.css'
-import '../fonts/__proximaNovaBlack/fonts__proximaNovaBlack.css'
 
 VerticalBanner.propTypes = {
     banner: PropTypes.object.isRequired,
@@ -16,7 +15,7 @@ export default function VerticalBanner(props) {
     let className = cn({
         'verticalBanner fonts__proximaNovaBlack': true,
         [props.className]: props.className,
-        [props.type]: props.type,
+        [`verticalBanner_${props.type}`]: props.type,
     });
     return (
         <Link to={props.banner.url} style={{backgroundImage: 'url(' + props.banner.bg + ')'}}
