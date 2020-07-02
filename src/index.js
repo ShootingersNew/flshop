0c78f137567d405cce51cd2f0cff5a5b047f94e1
shootingersNew <lebedevseergey@gmail.com>
2020-07-02 19:25:13 +0300

    removed react-trough,moved App to index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {store} from "./redux/store";
-import './index.css';
-import Routes from "./routes";
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from "react-router-dom"
+import {Provider} from 'react-redux'
+import {store} from "./redux/store"
+import './index.css'
+import Routes from "./routes"
+import App from "./App";
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <Routes/>
-        </Provider>
-    </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Router>
+            <App>
+                <Routes/>
+            </App>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,13 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import {MainPage} from "./pages/mainPage/MainPage";
 import Goods from "./pages/good/Goods";
-import App from "./App";
 import CartPage from "./pages/cartPage/CartPage";
 import ListingPage from "./pages/listingPage/ListingPage";
-import {ThroughProvider} from 'react-through';
 
 
 export default function Routes() {
     return (
-        <ThroughProvider>
-            <Router>
-                <App>
-                    {/*хедер и футер здесь для того, чтобы не рендерить их на определенных страницах при необходимости*/}
                     <Switch>
                         <Route exact path={"/"}>
                             <MainPage/>
@@ -22,9 +16,7 @@ export default function Routes() {
                         <Route path={'/cart'} component={CartPage}/>
                         <Route path={'/catalog'} component={ListingPage}/>
                     </Switch>
-                </App>
-            </Router>
-        </ThroughProvider>
+
     )
 
 }
