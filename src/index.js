62c28fc00fd9ccf2420ea37894d38fa0a49577d8
shootingersNew <lebedevseergey@gmail.com>
2020-07-02 19:26:23 +0300

    refactored imports in index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from "react-router-dom"
 import {Provider} from 'react-redux'
 import {store} from "./redux/store"
-import './index.css'
+//comps
 import Routes from "./routes"
 import App from "./App";
+//styles
+import './index.css'
+//other
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
