ec4988cde9985b9721a40d3b07a183e5f746a033
shootingersNew <lebedevseergey@gmail.com>
2020-07-02 16:46:23 +0300

    ListingPage.js 1)added function to count opened filters in Filter comp, if less than 1 banner is open, Aside Banners does not display a banner, 2)Container & Breadcrumbs replaced by Main 3)changed id & classname of asidebanners

--- src/pages/listingPage/ListingPage.js
+++ src/pages/listingPage/ListingPage.js
@@ -2,8 +2,6 @@
 import React from "react"
 //comps
 import withBDItemsConnect from "../../hoc/withBDItemsConnect"
-import Container from "../../common.blocks/container/Container"
-import Breadcrumbs from "../../common.blocks/breadcrumbs/Breadcrumbs"
 import Showcase from "../../common.blocks/showcase/Showcase"
 import Main from "../../common.blocks/main/Main"
 import ListingSidebar from "../../common.blocks/listingSidebar/ListingSidebar"
@@ -18,13 +16,15 @@ class ListingPage extends React.Component {
         this.state = {
             showWithSale: true,
             checkboxes: {},
-            price: {value: {min: 1000, max: 6000}}
+            price: {value: {min: 1000, max: 6000}},
+            filtersOpened: 1
 
         };
         this.setFilters.bind(this);
         this.getFilterTagsToArr.bind(this);
         this.uncheckCheckbox.bind(this);
         this.uncheckAllCheckboxes.bind(this);
+        this.countOpenedFilters.bind(this)
     }
 
     setFilters = (obj) => {
@@ -33,7 +33,9 @@ class ListingPage extends React.Component {
     uncheckCheckbox = (name, val) => {
         //if type of checkboxes is radio, their values are array. Default-string
         if (typeof this.state.checkboxes[name] === "string") {
-            this.setState({checkboxes: {...this.state.checkboxes, [name]: ''}})
+            let newArr = this.state.checkboxes;
+            delete newArr[name];
+            this.setState({checkboxes: newArr})
         } else {
             //filter array
             let newArr = this.state.checkboxes[name].filter((filterVal) => {
@@ -42,12 +44,21 @@ class ListingPage extends React.Component {
             this.setState({checkboxes: {...this.state.checkboxes, [name]: newArr}})
         }
     };
+
     uncheckAllCheckboxes = (e) => {
         e.preventDefault();
         this.setState({
             checkboxes: {}
         })
     };
+
+    //this function to count opened filters in Filter comp,
+    // if less than 1 banner is open, Aside Banners does not display a banner
+    countOpenedFilters = (i) => {
+        let newCounter = this.state.filtersOpened + i;
+        this.setState({filtersOpened: newCounter})
+    };
+
     getFilterTagsToArr = () => {
         const {checkboxes} = this.state;
         const categoriesJson = require('../../config/json/categories');
@@ -76,40 +87,50 @@ class ListingPage extends React.Component {
     render() {
         const filteredItems = new FilterItems(this.state, this.props.goods).filteredArr();
         return (
-            <Container>
-                <Breadcrumbs
-                    items={
+            <React.Fragment>
+                <Main
+                    className={'main_listing'}
+                    container={true}
+                    breadcrumbsArray={
                         [
                             {title: 'Главная', path: '/'},
                             {title: 'Каталог'}
                         ]
                     }
-                />
-                {/*aside*/}
-                <ListingSidebar>
-                    <Filters
-                        setFilters={this.setFilters}
-                        filtersState={this.state}
-                        uncheckAllCheckboxes={this.uncheckAllCheckboxes}
-                    />
-                    <AsideBanners
-                        id={'cart'}
-                        count={1}
-                    />
-                </ListingSidebar>
-                <Main className={'main_inlineBlock main_listing'}>
-                    <Showcase
-                        counter={filteredItems.length}
-                        showcaseType={'listing'}
-                        header={'Букеты'}
-                        idx={'allItems'}
-                        goods={filteredItems}
-                        filterTags={this.getFilterTagsToArr()}
-                        uncheckCheckbox={this.uncheckCheckbox}
-                    />
-                </Main>
+                    content={
+                        <React.Fragment>
+                            <Showcase
+                                counter={filteredItems.length}
+                                showcaseType={'listing'}
+                                header={'Букеты'}
+                                idx={'allItems'}
+                                goods={filteredItems}
+                                filterTags={this.getFilterTagsToArr()}
+                                uncheckCheckbox={this.uncheckCheckbox}
+                            />
+                        </React.Fragment>
+                    }
+                    aside={
+                        <ListingSidebar>
+                            <Filters
+                                setFilters={this.setFilters}
+                                filtersState={this.state}
+                                uncheckAllCheckboxes={this.uncheckAllCheckboxes}
+                                countOpenedFilters={this.countOpenedFilters}
+                            />
+                            {/*if opened more than 2 filter fieldsets && 7 or more items in showcase, then the banners are not displayed*/}
+                            <AsideBanners
+                                id={'listing'}
+                                count={this.state.filtersOpened < 2 && filteredItems.length >= 7 ? 1 : 0}
+                                type={'tall'}
+                                className={'listingSidebar__aside'}
+                            />
+                        </ListingSidebar>
+                    }
 
-            </Container>
+                />
+            </React.Fragment>
+
         )
     }
 }
