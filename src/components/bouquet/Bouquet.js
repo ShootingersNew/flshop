d3a0975ad652756ee26a59cccc2df52071237303
shootingersNew <lebedevseergey@gmail.com>
2020-07-13 14:30:18 +0300

    changes - the link is now the size of the entire product area

--- src/components/bouquet/Bouquet.js
+++ src/components/bouquet/Bouquet.js
@@ -2,6 +2,7 @@
 import React from "react"
 import {Link} from "react-router-dom"
 import PropTypes from 'prop-types'
+import cn from 'classnames'
 //comps
 import withCartConnect from "../../hoc/withCartConnect"
 //utils
@@ -15,9 +16,12 @@ Bouquet.propTypes = {
     addInCart: PropTypes.func.isRequired,
     classname: PropTypes.string,
     mod: PropTypes.string,
+    itemAmount: PropTypes.number
 };
 
 function Bouquet(props) {
+    const buttonRef = React.createRef();
+
     function increaseQuantity() {
         const quantity = props.item.amount + 1;
         props.changeQuantity(props.item.id, quantity)
@@ -30,20 +34,23 @@ function Bouquet(props) {
         }
     }
 
-    let classes = 'bouquet';
-    if (props.mod) {
-        classes = classes + ' ' + classes + '_' + props.mod
-    }
+    //prevent clicking on the link if e.target ==== add-to-cart button
+
+    const bouquetClasses = cn({
+        bouquet: true,
+        [`bouquet_${props.mod}`]: props.mod
+    });
+    const decreaseButtonClasses = cn({
+        'bouquet__counterButton  bouquet__counterButton_left icon-svg__minus': true,
+        'bouquet__counterButton_active': props.item.amount !== 1
+    });
+
     return (
-        <article className={classes + ' ' + props.classname}>
+        <article className={bouquetClasses}>
+            {/*if props.mod === type_cart then the content is rendered without a link*/}
+
             <main className="bouquet__main">
-                {props.mod && props.mod === 'type_cart' ?
-                    <Link to={'/goods/' + props.item.type + '/' + props.item.id}>
-                        <img className={'bouquet__image'} src={props.item.src} alt={props.item.name}/>
-                    </Link>
-                    :
-                    <img className={'bouquet__image'} src={props.item.src} alt={props.item.name}/>
-                }
+                <img className={'bouquet__image'} src={props.item.src} alt={props.item.name}/>
                 <div className="bouquet__info">
                     <div className="bouquet__price-info">
                         <span className="bouquet__price fonts__proximaNovaBold">{regExpPrice(props.item.price)}р</span>
@@ -65,42 +72,57 @@ function Bouquet(props) {
 
                 </div>
             </main>
-            {/*controls for cart bouquets*/}
-            {props.mod && props.mod === 'type_cart' ?
-                <React.Fragment>
-                    <div className="bouquet__controls">
-                        <div className="bouquet__counter">
+            {
+                props.mod && props.mod === 'type_cart' ?
+                    <React.Fragment>
+                        <div className="bouquet__controls">
+                            <div className="bouquet__counter">
                         <span onClick={() => {
                             decreaseQuantity()
-                        }} className={"bouquet__counterButton bouquet__counterButton_left icon-svg__minus"}/>
-                            <span className={"bouquet__counterNumber"}>{props.item.amount}</span>
-                            <span onClick={() => {
-                                increaseQuantity()
-                            }} className={"bouquet__counterButton bouquet__counterButton_right icon-svg__plus"}/>
+                        }} className={decreaseButtonClasses}/>
+                                <span className={"bouquet__counterNumber"}>{props.item.amount}</span>
+                                <span onClick={() => {
+                                    increaseQuantity()
+                                }} className={"bouquet__counterButton bouquet__counterButton_right icon-svg__plus"}/>
+                            </div>
+                            <div className="bouquet__sumPrice fonts__proximaNovaBold">
+                                {regExpPrice(props.item.sumPrice)}р
+                            </div>
                         </div>
-                        <div className="bouquet__sumPrice fonts__proximaNovaBold">
-                            {regExpPrice(props.item.sumPrice)}р
-                        </div>
-                    </div>
-                    <span onClick={() => {
-                        props.removeItem(props.item.id)
-                    }} className="bouquet__delete icon-svg__cross"/>
-                </React.Fragment>
-                :
-                //default footer
-                <footer className={'bouquet__footer'}>
-                    <Link className={'link'} to={'/goods/' + props.item.type + '/' + props.item.id}>Подробнее</Link>
-
-                    <button onClick={() => {
-                        props.addInCart(props.item)
-                    }} className="bouquet__cartButton" disabled={props.checkInCart(props.item.id)}>
-                        <span className="icon-svg__plus bouquet__plus"/>
-                        <span className="icon-svg__cartico bouquet__cart"/>
-                    </button>
-                </footer>
+                        <span onClick={() => {
+                            props.removeItem(props.item.id)
+                        }} className="bouquet__delete icon-svg__cross"/>
+                    </React.Fragment>
+                    :
+                    //default footer
+                    <React.Fragment>
+                        <footer className={'bouquet__footer'}>
+                            <Link className={'link'}
+                                  to={'/goods/' + props.item.type + '/' + props.item.id}>Подробнее</Link>
+                            <ButtonWithRefs ref={buttonRef} {...props}/>
+                        </footer>
+                        <Link
+                            className={'bouquet__link'}
+                            to={'/goods/' + props.item.type + '/' + props.item.id}
+                        />
+                    </React.Fragment>
             }
         </article>
+
     )
 }
+
+const ButtonWithRefs = React.forwardRef((props, ref) => (
+    <button
+        ref={ref}
+        className="bouquet__cartButton"
+        onClick={() => {
+            props.addInCart(props.item)
+        }}
+        disabled={props.checkInCart(props.item.id)}>
+        <span className="icon-svg__plus bouquet__plus"/>
+        <span className="icon-svg__cartico bouquet__cart"/>
+    </button>
+));
 
 export default withCartConnect(Bouquet)
