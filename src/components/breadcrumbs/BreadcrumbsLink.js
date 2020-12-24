8ace83f7de7d0e2326838fe6618780caee6d757d
shootingersNew <lebedevseergey@gmail.com>
2020-12-24 17:00:47 +0300

    small code refactor

--- src/components/breadcrumbs/BreadcrumbsLink.js
+++ src/components/breadcrumbs/BreadcrumbsLink.js
@@ -4,10 +4,11 @@ import {Link} from "react-router-dom"
 export default function BreadcrumbsLink(props) {
     return (
         <React.Fragment>
-            {!props.active ?
-                <Link className={'breadcrumbs__link'} to={props.path}>{props.children}</Link>
-                :
-                <span className={'breadcrumbs__active'}>{props.children}</span>
+            {
+                !props.active ?
+                    <Link to={props.path} className={'breadcrumbs__link'}>{props.children}</Link>
+                    :
+                    <span className={'breadcrumbs__active'}>{props.children}</span>
             }
         </React.Fragment>
     )
